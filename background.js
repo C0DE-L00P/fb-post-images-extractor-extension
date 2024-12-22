ff376d3f1b7d81316919368a0269f7e8c64f15c9
Mohamed Gamal <mohamedgyaseen@gmail.com>
2024-12-22 03:23:27 +0200

    Done

--- background.js
+++ background.js
@@ -1,55 +1,66 @@
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: startScanning
+    function: startScanning,
+    args: [tab.id]
   });
 });
 
 // Listen for messages from the content script
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'downloadImage') {
+    const filename = request.imageUrl.split('/').pop().split('?')[0];
     chrome.downloads.download({
       url: request.imageUrl,
-      filename: `fb_images/image_${Date.now()}.jpg`
+      filename: `fb_images/${filename}`
     });
+  } else if (request.action === 'closeTab') {
+    chrome.tabs.remove(request.tabId);
   }
 });
 
-function startScanning() {
+function startScanning(tabId) {
   let downloadedUrls = new Set();
+  let lastImageCount = 0;
   
   function scanAndDownload() {
     const images = document.querySelectorAll('[data-visualcompletion="media-vc-image"]');
-    let hasNewImages = false;
+    let newImagesCount = 0;
 
-    for (let img of images) {
+    // Download new images
+    images.forEach(img => {
       const imgUrl = img.src;
-      if (!downloadedUrls.has(imgUrl)) {
-        hasNewImages = true;
-        downloadedUrls.add(imgUrl);
-        
-        // Send message to background script to download the image
+      const filename = imgUrl.split('/').pop().split('?')[0];
+      if (!downloadedUrls.has(filename) && imgUrl) {
+        downloadedUrls.add(filename);
+        newImagesCount++;
         chrome.runtime.sendMessage({
           action: 'downloadImage',
           imageUrl: imgUrl
         });
       }
-    }
+    });
 
-    if (!hasNewImages) {
-      console.log('No new images found, stopping...');
-      return;
-    }
-
-    // Click next button after a delay to allow downloads to start
-    setTimeout(() => {
+    // If we found new images, continue to next page
+    if (newImagesCount > 0) {
+      lastImageCount = newImagesCount;
       const nextButton = document.querySelector('[style*="background-position: 0px -25px; background-size: auto; width: 24px; height: 24px; background-repeat: no-repeat; display: inline-block;"]');
       if (nextButton) {
         nextButton.click();
-        // Wait for page to load new images
-        setTimeout(scanAndDownload, 2000);
+        setTimeout(scanAndDownload, 500); // Small delay to let new images load
       }
-    }, 1000);
+    } 
+    // If we didn't find new images and last time we did, we're done
+    else if (lastImageCount > 0) {
+      chrome.runtime.sendMessage({
+        action: 'closeTab',
+        tabId: tabId
+      });
+    } 
+    // If we haven't found any images yet, try again
+    else {
+      setTimeout(scanAndDownload, 100);
+    }
   }
 
   scanAndDownload();
