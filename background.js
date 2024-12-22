f02c9ecd1963d9149bff5ce82a2be99b350426f8
Mohamed Gamal <mohamedgyaseen@gmail.com>
2024-12-22 03:45:01 +0200

    Convert to pdf after download

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+importScripts('jspdf.umd.min.js');
+
 chrome.action.onClicked.addListener((tab) => {
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
@@ -6,18 +8,68 @@ chrome.action.onClicked.addListener((tab) => {
   });
 });
 
-// Listen for messages from the content script
+let imagesToProcess = [];
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'downloadImage') {
     const filename = request.imageUrl.split('/').pop().split('?')[0];
+    imagesToProcess.push({
+      url: request.imageUrl,
+      filename: filename
+    });
     chrome.downloads.download({
       url: request.imageUrl,
       filename: `fb_images/${filename}`
     });
   } else if (request.action === 'closeTab') {
-    chrome.tabs.remove(request.tabId);
+    createPDF().then(() => {
+      chrome.tabs.remove(request.tabId);
+    });
   }
 });
+
+async function createPDF() {
+  const { jsPDF } = globalThis.jspdf;
+  const doc = new jsPDF('p', 'px', 'a4');
+  const pageWidth = doc.internal.pageSize.getWidth();
+  const pageHeight = doc.internal.pageSize.getHeight();
+
+  for (let i = 0; i < imagesToProcess.length; i++) {
+    const imgData = imagesToProcess[i];
+    try {
+      const response = await fetch(imgData.url);
+      const blob = await response.blob();
+      const base64data = await convertBlobToBase64(blob);
+
+      if (i > 0) {
+        doc.addPage();
+      }
+
+      // Add image at full page width
+      doc.addImage(base64data, 'JPEG', 0, 0, pageWidth, pageHeight, undefined, 'FAST');
+
+    } catch (error) {
+      console.error('Error processing image:', error);
+    }
+  }
+
+  // Convert PDF to base64 and download
+  const pdfBase64 = doc.output('datauristring');
+  chrome.downloads.download({
+    url: pdfBase64,
+    filename: 'facebook_images.pdf'
+  });
+  imagesToProcess = [];
+}
+
+function convertBlobToBase64(blob) {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onloadend = () => resolve(reader.result);
+    reader.onerror = reject;
+    reader.readAsDataURL(blob);
+  });
+}
 
 function startScanning(tabId) {
   let downloadedUrls = new Set();
@@ -27,7 +79,6 @@ function startScanning(tabId) {
     const images = document.querySelectorAll('[data-visualcompletion="media-vc-image"]');
     let newImagesCount = 0;
 
-    // Download new images
     images.forEach(img => {
       const imgUrl = img.src;
       const filename = imgUrl.split('/').pop().split('?')[0];
@@ -41,24 +92,19 @@ function startScanning(tabId) {
       }
     });
 
-    // If we found new images, continue to next page
     if (newImagesCount > 0) {
       lastImageCount = newImagesCount;
       const nextButton = document.querySelector('[style*="background-position: 0px -25px; background-size: auto; width: 24px; height: 24px; background-repeat: no-repeat; display: inline-block;"]');
       if (nextButton) {
         nextButton.click();
-        setTimeout(scanAndDownload, 500); // Small delay to let new images load
+        setTimeout(scanAndDownload, 500);
       }
-    } 
-    // If we didn't find new images and last time we did, we're done
-    else if (lastImageCount > 0) {
+    } else if (lastImageCount > 0) {
       chrome.runtime.sendMessage({
         action: 'closeTab',
         tabId: tabId
       });
-    } 
-    // If we haven't found any images yet, try again
-    else {
+    } else {
       setTimeout(scanAndDownload, 100);
     }
   }
